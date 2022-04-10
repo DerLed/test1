681961e71ea2dd59b954fb1002148518c30bafe4
DerLed <48630545+DerLed@users.noreply.github.com>
2022-04-10 22:03:25 +0300

    edit

---
+++ src/com/company/ConvertNumbers.java
@@ -0,0 +1,110 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Перевод чисел из десятичной в шестнадцатиричную,
+ * из десятичной в двоичную и
+ * из двоичной в десятичную.
+ */
+public class ConvertNumbers {
+    //Внутренний сканнер
+    private final Scanner scanner;
+
+    //Строка чисел прочитанная с клавиатуры
+    String numString;
+
+    //Регуляроное выражение для проверки двоичного числа
+    String regexBin = "^[0-1]*$";
+
+    //Регулярное выражение для проверки десятичного числа
+    String regexDec = "^[0-9]*$";
+
+    public ConvertNumbers(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    //Метод выводит меню выбора в консоль
+    private void printMenu() {
+        System.out.println("""
+                Конвертация чисел
+                Выберите режим, введите:
+                1 - для конвертации из 10 системы счисления в шеснадцатиричную
+                2 - для конвертации из 10 системы счисления в двоичную
+                3 - для конвертации из двоичной системы счисления в десятичную
+                4 - для выхода из программы""");
+    }
+
+    //Выбор режима работы программы
+    public void start() {
+        if (this.scanner != null) {
+            int key;
+            do {
+                printMenu();
+                System.out.print("Введите номер меню: ");
+                key = this.scanner.nextInt();
+                switch (key) {
+                    case 1:
+                        myDecToHex();
+                        break;
+                    case 2:
+                        myDecToBin();
+                        break;
+                    case 3:
+                        myBinToDec();
+                        break;
+                    case 4:
+                        System.out.println("Завершение программы...");
+                        break;
+                    default:
+                        System.out.println("Вы ввели неверное значение меню...\n");
+                }
+            } while (key != 4);
+        }
+    }
+
+    //Перевод десятичного значения в двоичное
+    private void myDecToBin() {
+        System.out.println("Введите десятичное число для перевода в двоичную систему счисления " +
+                "или введите \"r\" для возврата");
+        String num = checkValueOut(regexDec);
+        if(num.equals("r")) return;
+        System.out.printf("Число %s в двоичной системе: %s\n\n", num, Integer.toBinaryString(Integer.parseInt(num)));
+    }
+
+    //Перевод десятичного значения в шестнадцатиричное
+    private void myDecToHex() {
+        System.out.println("Введите десятичное число для перевода в шеснадцатиричную систему счисления " +
+                "или введите \"r\" для возврата");
+        String num = checkValueOut(regexDec);
+        if(num.equals("r")) return;
+        System.out.printf("Число %s в шеснадцатиричной системе: %s\n\n", num, Integer.toHexString(Integer.parseInt(num)));
+    }
+
+    //Перевеод двоичного в десятичное
+    private void myBinToDec() {
+        System.out.println("Введите двоичное число для перевода в десятичную систему счисления " +
+                "или введите \"r\" для возврата");
+        String num = checkValueOut(regexBin);
+        if(num.equals("r")) return;
+        System.out.printf("Число %s в десятичной системе: %d\n\n", num, Integer.parseInt(num, 2));
+    }
+
+    /**
+     * Метод принимает регулярное выражение, производит проверку на корректность ввода
+     * исходя из заданого регулярного выражения.
+     * Если ввод корректный возвращает введеные данные в String,
+     * если ввод не корректный просит пользователя повторить ввод ли бо ввести символ "r" для выхода из данного режима.
+     */
+    private String checkValueOut(String regex) {
+        boolean isCorrect;
+        do {
+            numString = scanner.next();
+            if (numString.equals("r")) return "r";
+            isCorrect = numString.matches(regex);
+            if (!isCorrect) System.out.println("Введено не корректное число повторите ввод или введите \"r\" для возврата");
+        } while (!isCorrect);
+        return numString;
+    }
+}
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,70 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("""
-                Конвертация чисел
-                выберите режим, введите:
-                16 - для конвертации из 10 системы счисления в шеснадцатиричную
-                2 - для конвертации из 10 системы счисления в двоичную
-                10 - для конвертации из двоичной системы счисления в десятичную
-                -1 - для выхода из программы""");
-        Scanner in = new Scanner(System.in);
-        int option = in.nextInt();
-
-        while (option != -1) {
-            if (option == 16) {
-                myToHex();
-                break;
-            } else if (option == 2) {
-                myDecToBin();
-                break;
-            } else if (option == 10) {
-                myToBinary();
-                break;
-            } else {
-                System.out.println("Повторите ввод");
-                option = in.nextInt();
-            }
-        }
-
-        System.out.println("Программа завершает работу");
-        in.close();
-
-
+        Scanner scanner = new Scanner(System.in);
+        new ConvertNumbers(scanner).start();
+        scanner.close();
     }
-
-    private static void myDecToBin() {
-        System.out.println("Введите десятичное число для перевода в двоичную систему исчисления");
-        Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        in.close();
-        System.out.printf("Число %d в двоичной системе: ", num);
-        System.out.println(Integer.toBinaryString(num));
-    }
-
-    private static void myToBinary() {
-        System.out.println("Введите двоичное число для перевода в десятичную систему исчисления");
-        Scanner in = new Scanner(System.in);
-        String numString = in.next();
-        in.close();
-
-        int nBinary = 0;
-        for (int i = numString.length() - 1, j = 1; i >= 0; i--, j *= 2) {
-            nBinary += (numString.charAt(i) - '0') * j;
-        }
-        System.out.printf("Число %s в десятичной системе: %d", numString, nBinary);
-
-
-    }
-
-    private static void myToHex() {
-        System.out.println("Введите десятичное число для перевода в шеснадцатиричное");
-        Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        in.close();
-        System.out.printf("Число %d в шеснадцатиричной системе: ", num);
-        System.out.println(Integer.toHexString(num));
-    }
-
 
 }
